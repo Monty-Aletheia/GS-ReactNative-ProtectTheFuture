b75ed49e2a2d50b408142ea2ce6bd8e911b5a3c0
Pedro Lucca <pedroluccaprofissional@gmail.com>
2025-05-31 13:25:05 -0300

    fix(firebase): bypass TS check to access app.name from firebaseConfig

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,11 @@ import { StyleSheet } from 'react-native'
 import React from 'react'
 import '../global';
 import { Slot } from 'expo-router';
+// @ts-ignore
+import { app } from '../firebaseConfig'
+//Não sei dizer direito o pq, mas precisa disso.
+// @ts-ignore
+console.log(app.name);
 
 const AppLayout = () => {
   return (
